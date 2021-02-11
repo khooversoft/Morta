678ead1195705633a915f0d97b8c822d887006fa
Kelvin <v-kelhoo@microsoft.com>
2021-02-11 14:12:31 -0800

    ALl test pass for language

--- Src/Tools/Toolbox.Language.Test/PropertyTests.cs
+++ Src/Tools/Toolbox.Language.Test/PropertyTests.cs
@@ -26,21 +26,18 @@ namespace Toolbox.Language.Test
 
             var properties = new CodeBlock<TestTokenType>()
                 + LanguageSyntax.LeftBrace
-                + assignment
-
-                + (new Optional<TestTokenType>()
-                    + LanguageSyntax.With
-                    + propertiesReference
-                )
 
                 + (new Optional<TestTokenType>()
                     + (new Repeat<TestTokenType>()
-                        + LanguageSyntax.Comma
                         + assignment
 
                         + (new Optional<TestTokenType>()
                             + LanguageSyntax.With
                             + propertiesReference
+                        )
+
+                        + (new Optional<TestTokenType>()
+                            + LanguageSyntax.Comma
                         )
                     )
                 )
@@ -373,21 +370,17 @@ namespace Toolbox.Language.Test
         }
 
         [Fact]
-        public void MultipleObjectsWithProperty_ShouldPass()
+        public void WidthMultipleEmbedded_ShouldPass()
         {
             var commands = new[]
             {
                 "objectName = {",
-                    "Name1=Value1,",
-                    "Name2 =Value2,",
-                    "Name3 = Value3 with {",
-                        "Name4 = Value4,",
-                        "Name5 = Value5 with {",
-                            "Name6 = Value6",
+                    "Name1 = Value1 with {",
+                        "Name2 = Value2 with {",
+                            "Name3 = Value3 with {",
+                                "Name4 = Value4",
+                            "}",
                         "}",
-                    "},",
-                    "Name7 = {",
-                        "Name4 = Value4",
                     "}",
                 "};"
             };
@@ -411,8 +404,171 @@ namespace Toolbox.Language.Test
                 new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name2"),
                 new SymbolToken<TestTokenType>(TestTokenType.Equal),
                 new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value2"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name3"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value3"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name4"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value4"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
                 new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
 
+                new SymbolToken<TestTokenType>(TestTokenType.SemiColon),
+            };
+
+            Enumerable.SequenceEqual(response.Nodes!, matchList).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WidthMultipleEmbedded_Patther2_ShouldPass()
+        {
+            var commands = new[]
+            {
+                "objectName = {",
+                    "Name1 = Value1 with {",
+                        "Name2 = Value2 with {",
+                            "Name3 = Value3 with {",
+                                "Name4 = Value4",
+                            "},",
+                         "Name5 = Value5",
+                        "}",
+                    "}",
+                "};"
+            };
+
+            var parser = new SymbolParser<TestTokenType>(_processRules);
+
+            SymbolParserResponse<TestTokenType> response = parser.Parse(commands);
+            response.Nodes.Should().NotBeNull();
+
+            var matchList = new ISymbolToken[]
+            {
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "objectName"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name1"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value1"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name2"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value2"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name3"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value3"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name4"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value4"),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+
+                new SymbolToken<TestTokenType>(TestTokenType.Comma),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name5"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value5"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+
+                new SymbolToken<TestTokenType>(TestTokenType.SemiColon),
+            };
+
+            Enumerable.SequenceEqual(response.Nodes!, matchList).Should().BeTrue();
+        }
+
+        [Fact]
+        public void MultipleObjectsWithProperty_ShouldPass()
+        {
+            var commands = new[]
+            {
+                "objectName = {",
+                    "Name1=Value1,",
+                    "Name2 =Value2,",
+                    "Name3 = Value3 with {",
+                        "Name4 = Value4,",
+                        "Name5 = Value5 with {",
+                            "Name6 = Value6",
+                        "}",
+                    "},",
+                    "Name7 = Value7 with {",
+                        "Name8= Value8",
+                    "}",
+                "};"
+            };
+
+            var parser = new SymbolParser<TestTokenType>(_processRules);
+
+            SymbolParserResponse<TestTokenType> response = parser.Parse(commands);
+            response.Nodes.Should().NotBeNull();
+
+            var matchList = new ISymbolToken[]
+            {
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "objectName"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name1"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value1"),
+                new SymbolToken<TestTokenType>(TestTokenType.Comma),
+
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name2"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value2"),
+                new SymbolToken<TestTokenType>(TestTokenType.Comma),
+
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name3"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value3"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name4"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value4"),
+                new SymbolToken<TestTokenType>(TestTokenType.Comma),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name5"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value5"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name6"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value6"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
+                new SymbolToken<TestTokenType>(TestTokenType.Comma),
+
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name7"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value7"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.With),
+                new SymbolToken<TestTokenType>(TestTokenType.LeftBrace),
+                new SymbolValue<TestTokenType>(TestTokenType.VariableName, "Name8"),
+                new SymbolToken<TestTokenType>(TestTokenType.Equal),
+                new SymbolValue<TestTokenType>(TestTokenType.Constant, "Value8"),
+
+                new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
                 new SymbolToken<TestTokenType>(TestTokenType.RightBrace),
                 new SymbolToken<TestTokenType>(TestTokenType.SemiColon),
             };
